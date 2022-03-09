f97af58983f971132310ae26c121d7800a09f124
Brandon-bass <brandonbass@u.boisestate.edu>
2022-03-08 21:55:24 -0700

    Basic fullpage header

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -6,8 +8,10 @@ export default function Home() {
         <h1 className={styles.h1}>
         <header className={styles.header}>
           Gimm Studio
+
           </header>
         </h1>
+        <p>lorim ipsum</p>
       </main>
       
     
